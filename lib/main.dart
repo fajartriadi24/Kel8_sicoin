864012e3cb46f67ec249cdddc399a19e3f6445fd
Fajar Triadi <fajartriadi244@gmail.com>
2026-01-11 12:32:24 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
-// 1. IMPOR PACKAGE UNTUK INISIALISASI
+// 1. Import tambahan WAJIB agar Web tidak Error
+import 'package:flutter/foundation.dart'; // Untuk cek kIsWeb
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart'; // Paket Web Database
+import 'package:sqflite/sqflite.dart'; // Paket Database utama
+
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:sicoin/welcome_page.dart';
 
-// 2. UBAH main() MENJADI async dan TAMBAHKAN KODE INISIALISASI
 void main() async {
-  // Baris ini wajib ada di dalam main() yang async sebelum runApp
+  // Wajib ada di baris pertama
   WidgetsFlutterBinding.ensureInitialized();
 
-  // **PERBAIKAN UTAMA ADA DI SINI**
-  // Inisialisasi data lokalisasi untuk bahasa Indonesia
+  // 2. Inisialisasi Format Tanggal (Indonesia)
   await initializeDateFormatting('id_ID', null);
+
+  // 3. LOGIKA PERBAIKAN ERROR WEB (PENTING!)
+  // Cek: Jika aplikasi dijalankan di browser (Web)
+  if (kIsWeb) {
+    // Gunakan factory database khusus Web
+    databaseFactory = databaseFactoryFfiWeb;
+  }
 
   runApp(const MyApp());
 }
@@ -25,9 +34,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
-        fontFamily: 'Poppins', // Jika Anda menggunakan font custom
+        fontFamily: 'Poppins', 
       ),
-      debugShowCheckedModeBanner: false, // Menghilangkan banner debug
+      debugShowCheckedModeBanner: false, 
       home: const WelcomePage(),
     );
   }
